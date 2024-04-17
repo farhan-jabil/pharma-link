a27992f0da4bf1224b0ab1f51bc16dec61ac6da1
farhan-jabil <farhanhasan295@gmail.com>
2024-04-18 03:13:10 +0600

    working on login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <>
-     
+     <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+      </Routes>
+     </Router>
     </>
   );
 }
