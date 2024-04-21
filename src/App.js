51f8a61d0968854812d9bd9b534c14a40fe9df83
farhan-jabil <farhanhasan295@gmail.com>
2024-04-21 14:38:56 +0600

    login page with responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './pages/login/Login';
+import Login from './pages/login/Loginn';
+import Dashboard from './pages/admin/Dashboard';
+
+
 
 function App() {
   return (
@@ -8,6 +11,7 @@ function App() {
      <Router>
       <Routes>
         <Route path="/" element={<Login/>}/>
+        <Route path='/adminDashboard' element={<Dashboard/>}></Route>
       </Routes>
      </Router>
     </>
