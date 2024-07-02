4a5ecc054ee10fd618c117f55854851ebbb1f86d
farhan-jabil <farhanhasan295@gmail.com>
2024-07-02 22:34:19 +0600

    adding backend folder

--- src/components/layouts/SidePanel.js
+++ src/components/layouts/SidePanel.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SidePanel = ({ children }) => {
-  const { pathname } = useLocation;
   return (
     <>
       <div className="">
